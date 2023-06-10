482dde58e3c3c1408028ff45056fcd81cd18f8c0
DennyBui <dbuidmd2018@gmail.com>
2023-06-09 19:29:36 -0700

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,11 @@ function App() {
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
 
   function handleQuestionAnswered(correct) {
+    console.log(currentQuestionId, questions.length)
     if (currentQuestionId < questions.length) {
       setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
     } else {
+      console.log("do you get here 2")
       setCurrentQuestion(null);
     }
     if (correct) {
